a61e57391456e845829b6a7f67592b8361a25138
Allen <allenlydiard@gmail.com>
2017-08-01 12:45:39 -0300

    Pushing to back up work at 12:45 PM

--- game/control.go
+++ game/control.go
@@ -1,28 +1,48 @@
 package game
 
-import "github.com/bwmarrin/discordgo"
+import (
+	"github.com/FM1337/Discord-CAH/utils"
+	"github.com/bwmarrin/discordgo"
+)
 
 // Start will start the game.
 func Start(s *discordgo.Session, m *discordgo.MessageCreate) {
-
+	if Running || Starting {
+		s.ChannelMessageSend(utils.Config.CAHChannelID, "A game is already running!")
+		return
+	}
+	// Initialize the data before adding players.
+	InitializeData()
 }
 
 // Pause will pause the game.
 func Pause(s *discordgo.Session, m *discordgo.MessageCreate) {
-
+	if !Running {
+		s.ChannelMessageSend(utils.Config.CAHChannelID, "No game is running!")
+		return
+	}
 }
 
 // Stop will stop the game
 func Stop(s *discordgo.Session, m *discordgo.MessageCreate) {
-
+	if !Running {
+		s.ChannelMessageSend(utils.Config.CAHChannelID, "No game is running!")
+		return
+	}
 }
 
 // Join will join you into the game
 func Join(s *discordgo.Session, m *discordgo.MessageCreate) {
-
+	if !Running {
+		s.ChannelMessageSend(utils.Config.CAHChannelID, "No game is running!")
+		return
+	}
 }
 
 // Leave will remove you from the game
 func Leave(s *discordgo.Session, m *discordgo.MessageCreate) {
-
+	if !Running {
+		s.ChannelMessageSend(utils.Config.CAHChannelID, "No game is running!")
+		return
+	}
 }
--- game/data.go
+++ game/data.go
@@ -1,25 +1,96 @@
 package game
 
+import (
+	"fmt"
+
+	"github.com/FM1337/Discord-CAH/cards"
+)
+
 // Player is a struct that holds a player's data.
 type Player struct {
-	PlayerName string
-	PlayerID   string
-	Zar        bool
-	Cards      []WhiteCard
+	PlayerName string      // The player's Discord user name.
+	PlayerID   string      // The player's Discord ID.
+	Zar        bool        // Is the player the card zar?
+	Cards      []WhiteCard // The player's hand.
 }
 
 // WhiteCard is a struct that holds the data about a white card.
 type WhiteCard struct {
-	CardID int
-	Index  int
-	Text   string
-	Blank  bool
-	taken  bool
+	CardID int    // The card's ID.
+	Index  int    // The card's index value
+	Text   string // The card's text.
+	Blank  bool   // Is the card a blank card?
+	taken  bool   // Is the card taken by a player?
 }
 
 // BlackCard is a struct that holds the data about a black card.
 type BlackCard struct {
-	CardID int
-	Text   string
-	Cards  int
+	CardID int    // The card's ID.
+	Text   string // The card's text
+	Cards  int    // The amount of cards to play for this card.
+}
+
+// RoundResults is a struct that contains data about the chosen cards by players.
+type RoundResults struct {
+	PlayerName  string      // The Player's Discord name.
+	PlayerID    string      // The Player's Discord ID.
+	PlayerCards []WhiteCard // Slice of WhiteCard struct.
+}
+
+// Bools
+// Starting is a bool to tell if the game is in the start up stage.
+var Starting bool
+
+// Running is a bool to tell if the game is running.
+var Running bool
+
+// Paused is a bool to tell if the game is paused.
+var Paused bool
+
+// Maps
+// BlackCards is a map of the BlackCard struct.
+var BlackCards map[int]BlackCard
+
+// WhiteCards is a map of the WhiteCard struct.
+var WhiteCards map[int]WhiteCard
+
+// InitializeData will prepare the maps and slices for the game
+func InitializeData() {
+	// First we do the black cards
+	BlackCards = make(map[int]BlackCard)
+	ImportBlackCards()
+	// Print the amount of Black Cards loaded into memory.
+	fmt.Printf("%d Black Cards loaded!\n", len(BlackCards))
+
+	// Then we do the white cards.
+	WhiteCards = make(map[int]WhiteCard)
+	ImportWhiteCards()
+	// Print the amount of White Cards loaded into memory.
+	fmt.Printf("%d White Cards loaded!\n", len(WhiteCards))
+}
+
+// ImportBlackCards will import the black cards.
+func ImportBlackCards() {
+	// Loop through the black cards imported at start up of the bot and
+	// add them to our map of black cards.
+	for i, card := range cards.CardList.BlackCards {
+		BlackCards[i] = BlackCard{
+			CardID: i,
+			Text:   card.CardText,
+			Cards:  card.Cards2Play,
+		}
+	}
+}
+
+// ImportWhiteCards will import the white cards.
+func ImportWhiteCards() {
+	// Loop through the white cards imported at start up of the bot and
+	// add them to our map of white cards.
+	for i, card := range cards.CardList.WhiteCards {
+		WhiteCards[i] = WhiteCard{
+			CardID: i,
+			Text:   card.CardText,
+			taken:  false,
+		}
+	}
 }
